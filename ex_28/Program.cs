20a59c54ff7d32cd2054a1d239b7ef0618348dd3
Tatiana Kalinina <tankakalinka@gmail.com>
2022-09-09 22:27:28 +0300

    add ex 28 Prod From 1 Until N

---
+++ ex_28/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и 
+// выдаёт произведение чисел от 1 до N.
+
+Console.Write("Input any number: ");
+int number = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(FindProduce());
+
+int FindProduce()
+{
+    int produce = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        produce = produce * i;
+    }
+    return produce;
+}
