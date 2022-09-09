8d11a47b3a2a33f3e3bdf8c4d28789fd89e4a841
Tatiana Kalinina <tankakalinka@gmail.com>
2022-09-09 22:26:05 +0300

    add ex 26 Digits Sum In Number

---
+++ ex_26/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и 
+// выдаёт количество цифр в числе.
+
+Console.Write("Input number N: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int count = 0;
+while (number > 0) {
+    number = number / 10; //оставить все, кроме последней цифры
+    count = count + 1;
+}
+Console.WriteLine(count);
